be31463632491238e662751748a70fd96908b25d
Johannes Lamberts <mail@j-lamberts.de>
2020-02-25 21:23:44 +0100

    chore: setup prettier config

--- server/api.js
+++ server/api.js
@@ -1,24 +1,24 @@
-const fetch = require("node-fetch");
-const url = require("url");
+const fetch = require('node-fetch')
+const url = require('url')
 
 async function fetchFromApi(path, query = {}) {
   const requestUrl = url.format({
-    protocol: "https",
-    hostname: "api.themoviedb.org",
+    protocol: 'https',
+    hostname: 'api.themoviedb.org',
     pathname: `/3${path}`,
     query: {
       api_key: process.env.TOKEN,
-      ...query
-    }
-  });
-  console.info(`load ${requestUrl}`);
-  const res = await fetch(requestUrl);
+      ...query,
+    },
+  })
+  console.info(`load ${requestUrl}`)
+  const res = await fetch(requestUrl)
   if (res.status !== 200) {
-    throw new Error(`Status is ${res.status}`);
+    throw new Error(`Status is ${res.status}`)
   }
-  return res.json();
+  return res.json()
 }
 
 module.exports = {
-  fetchFromApi
-};
+  fetchFromApi,
+}
--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
 require('dotenv').config()
-const path = require("path");
-const fs = require("fs");
-const { ApolloServer } = require("apollo-server-express");
-const express = require("express");
-const resolvers = require("./resolvers");
-const gql = require("graphql-tag");
+const path = require('path')
+const fs = require('fs')
+const { ApolloServer } = require('apollo-server-express')
+const express = require('express')
+const resolvers = require('./resolvers')
+const gql = require('graphql-tag')
 
-const typeDefs = fs.readFileSync(path.resolve(__dirname, "typeDefs.gql"));
+const typeDefs = fs.readFileSync(path.resolve(__dirname, 'typeDefs.gql'))
 
-const app = express();
+const app = express()
 
 const apollo = new ApolloServer({
   typeDefs: gql`
     ${typeDefs}
   `,
-  resolvers
-});
+  resolvers,
+})
 
-apollo.applyMiddleware({ app });
-app.listen({ port: 4000 });
+apollo.applyMiddleware({ app })
+app.listen({ port: 4000 })
 console.log('running on http://localhost:4000/graphql')
